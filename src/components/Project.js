24315a1d1af0ecd44af17d5963fac051bf2c13fe
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-11-18 14:02:29 -0600

    set up nav bar, set up and rendered project componenet

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,23 +1,56 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const Project = ({ projects, setProjects }) => {
 
     useEffect(() => {
-        const url = 'http://localhost:8000/api/projects';
-        fetch(url)
-          .then((res) => res.json())
-          .then((json) => setProjects (json))
-          .catch(console.error);
+        axios.get('http://localhost:8000/api/projects')
+        .then(res => setProjects(res.data)) //Why res.data?
+        .catch(console.error)
       }, []);
 
+    if(!projects){
+        return <h5>Loading...</h5>
+    }
 
     return (
-        <div>
-           {projects.map((project) => (
-              <div key={project._id}>{project.title}</div>
-           ))}
-        </div>
+        <>
+            <div>
+            <Jumbotron>
+                <Container>
+                    <Card.Body>
+                        <Card>
+                            <h2>{projects[0].title}</h2>
+                            <p>{projects[0].description}</p>
+                            <p>{projects[0].tasks}</p>
+                            <p>{projects[0].links}</p>
+                            <p>{projects[0].dueDate}</p>
+                        </Card>
+                        <Card>
+                            <h2>{projects[1].title}</h2>
+                            <p>{projects[1].description}</p>
+                            <p>{projects[1].tasks}</p>
+                            <p>{projects[1].links}</p>
+                            <p>{projects[1].dueDate}</p>
+                        </Card>
+                        <Card>
+                            <h2>{projects[2].title}</h2>
+                            <p>{projects[2].description}</p>
+                            <p>{projects[2].tasks}</p>
+                            <p>{projects[2].links}</p>
+                            <p>{projects[2].dueDate}</p>
+                        </Card>
+                    </Card.Body>
+                </Container>
+            </Jumbotron>
+            </div>
+        </>
     );
 };
 
